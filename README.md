ab188d36e484a04aa8ce4aa49742b5dc2091aed1
iceyheart <40313247+iceyheart@users.noreply.github.com>
2022-05-10 09:31:27 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# 云编译 N1 OpenWrt 固件
+
+**集成**
+- SSR+、passwall、passwall2、nps、zerotier、kms、timectrol、KMS，其它服务略
+
+**旁路网关模式**
+- DHCP关、wireless关
+- 默认IP 192.168.1.200 网关192.168.1.1，关闭桥接，开启IPV6，取消分配前缀
+
+- 源码来源： Lean 的 Openwrt 源码仓库 https://github.com/coolsnowwolf/lede
+- 脚本参考： https://github.com/huangqian8/Cloud-N1-OpenWrt | https://github.com/P3TERX/Actions-OpenWrt
---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd openwrt
+# wireless
+rm -rf files/etc/config/wireless
+rm -rf files/etc/modules.d/wireless_enable
+# Add luci-theme-opentomcat
+svn co https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+# Add luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#Add amlogic管理
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic
