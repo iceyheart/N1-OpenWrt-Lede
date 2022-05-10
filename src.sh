ac15695183ff3cf042eaedaa195455b54ca7c26a
iceyheart <40313247+iceyheart@users.noreply.github.com>
2022-05-10 09:39:17 +0800

    Update src.sh

--- src.sh
+++ src.sh
@@ -2,5 +2,5 @@
 cd openwrt
 # Add a feed source
 echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/iceyheart/openwrt-passwall5.5' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
