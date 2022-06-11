8b46749deee9f202de30f5486c243c0b923f5237
iceyheart <40313247+iceyheart@users.noreply.github.com>
2022-06-11 20:44:08 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,5 +16,3 @@ git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 #Add luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
-#Add luci-app-chinadns-ng
-#git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/lean/luci-app-chinadns-ng
