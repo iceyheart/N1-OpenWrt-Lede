334620fb5b9407c9429e84a5a3ad050327c10efa
iceyheart <40313247+iceyheart@users.noreply.github.com>
2024-02-29 21:05:45 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,11 +13,12 @@ rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 #Add amlogic管理
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic
+rm -rf package/lean/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/lean/luci-app-amlogic
 #Add luci-app-passwall
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
 #Add openclash
-#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/lean/luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash package/lean/luci-app-openclash
 #Add luci-app-smartdns
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 #Add luci-app-adguardhome
