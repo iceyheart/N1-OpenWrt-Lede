f19dd1360efbea7afabd5612f830312ba1bd067c
iceyheart <40313247+iceyheart@users.noreply.github.com>
2024-12-24 20:05:09 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,10 +15,6 @@ cd openwrt
 #Add amlogic管理
 #rm -rf package/lean/luci-app-amlogic
 #git clone https://github.com/ophub/luci-app-amlogic.git package/lean/luci-app-amlogic
-#Add packges
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-git pull
 #Add luci-app-passwall
 #git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
 #Add openclash
