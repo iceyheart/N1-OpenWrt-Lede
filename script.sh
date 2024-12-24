6c3df5644300a8706a5efdf546eb2b789269f8df
iceyheart <40313247+iceyheart@users.noreply.github.com>
2024-12-24 23:44:01 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,13 +10,13 @@ cd openwrt
 #wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 # Add luci-theme-argon
 #rm -rf package/lean/luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 #Add amlogic管理
 #rm -rf package/lean/luci-app-amlogic
 #git clone https://github.com/ophub/luci-app-amlogic.git package/lean/luci-app-amlogic
-#Add luci-app-passwall
-#git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
+Add luci-app-passwall
+git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 #Add openclash
 #svn co https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash package/lean/luci-app-openclash
 #Add luci-app-smartdns
