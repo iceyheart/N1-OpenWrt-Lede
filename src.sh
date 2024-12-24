161afd9e4a55d1940c1efd33d9fd38f0eea6d30c
iceyheart <40313247+iceyheart@users.noreply.github.com>
2024-12-24 23:41:51 +0800

    Update src.sh

--- src.sh
+++ src.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd openwrt
-# Add feed source
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# Add passwall
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
