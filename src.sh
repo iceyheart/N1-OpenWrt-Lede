2029f6c22cc9d27a1303e296e3ca914a147a5c17
iceyheart <40313247+iceyheart@users.noreply.github.com>
2024-12-25 09:53:28 +0800

    Delete src.sh

--- src.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd openwrt
-# Add passwall
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
