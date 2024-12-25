41b3f6a7eee20e1a548bee9c89ce6a9ab83cbe38
iceyheart <40313247+iceyheart@users.noreply.github.com>
2024-12-25 09:42:51 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,8 +10,7 @@ cd openwrt
 #wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 # Add luci-theme-argon
 #rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+git clone https://github.com/jerrykuku/luci-theme-argon.git
 #Add amlogic管理
 #rm -rf package/lean/luci-app-amlogic
 #git clone https://github.com/ophub/luci-app-amlogic.git package/lean/luci-app-amlogic
